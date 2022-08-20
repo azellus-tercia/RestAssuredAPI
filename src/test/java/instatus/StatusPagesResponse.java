9c82754f0e7b4d1976f0d9c33ccca2942101d1da
azellus-tercia <azellustercia@gmail.com>
2022-08-20 16:41:44 +0500

    Changed test to chain model.

---
+++ src/test/java/instatus/StatusPagesResponse.java
@@ -0,0 +1,37 @@
+package instatus;
+
+public class StatusPagesResponse {
+    public String id;
+    public String subdomain;
+    public String name;
+    public String logoUrl;
+    public String faviconUrl;
+    public String websiteUrl;
+    public String customDomain;
+    public String publicEmail;
+    public String twitter;
+    public String status;
+    public String subscribeBySms;
+    public String language;
+    public String useLargeHeader;
+    public String brandColor;
+    public String okColor;
+    public String disruptedColor;
+    public String degradedColor;
+    public String downColor;
+    public String noticeColor;
+    public String unknownColor;
+    public String googleAnalytics;
+    public String smsService;
+    public String htmlInMeta;
+    public String htmlAboveHeader;
+    public String htmlBelowHeader;
+    public String htmlAboveFooter;
+    public String htmlBelowFooter;
+    public String htmlBelowSummary;
+    public String launchDate;
+    public String cssGlobal;
+    public String onboarded;
+    public String createdAt;
+    public String updatedAt;
+}
